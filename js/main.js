f7f44f8c6726b26b864c2651dfeb59042d023594
Ramon Guilherme <ramon.guilherme@gunnebo.com>
2021-06-10 14:50:39 -0300

    Inclusão de arquivos de estudo .

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var nome = "Ramones";
+var idade = 29;
+var idade2 = 10;
+var frase = "Brasil é o melhor time do mundo"; 
+//alert("O senhor " + nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.toLowerCase());
